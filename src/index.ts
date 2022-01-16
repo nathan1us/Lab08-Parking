a9a611a2abc53c2dbd88ad62931f3a4b79550698
Georgi Slaveykov <geoslaveykov@gmail.com>
2022-01-16 08:15:54 +0200

    📚 Implemented documentation via Swagger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as bodyParser from 'body-parser';
 import express from 'express';
 import Logger from './lib/logger';
+import swaggerUi from 'swagger-ui-express';
+import swaggerConfig from '../swagger.json';
 
 import {
     createParkingLot,
@@ -17,6 +19,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerConfig, {
+        swaggerOptions: {
+            supportedSubmitMethods: []
+        }
+    })
+);
+
 app.get('/parking', getAllParkingLots);
 app.post('/parking', createParkingLot);
 
@@ -30,6 +41,7 @@ app.delete('/vehicle/:plate', removeVehicleByPlate)
 
 app.listen(3000, () => {
     Logger.info('🚀 Server ready at: http://localhost:3000');
-})
+    Logger.info('📚 API docs served at: http://localhost:3000/docs');
+});
 
 export default app;
